29eff1fdaf7eb763706e427f49252121fbe8c845
Winston de Jong <59544401+winston-de@users.noreply.github.com>
2024-06-07 17:50:23 -0400

    Updated for lesson

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ notes:
 - started with counter to get into tkinter
 - took exactly three hours to get through both
 - students appreciated implementing their own operators/functions, or coming up with new ones
+
+# paint app:
+![](cwc_paint.png)
+notes:
+- scrapped save due to needing to install ghostscript, plus had issues with artifacts in saved file
+- didn't have enough time to have students implement their own features, may want to narrow it down in the future (scrapping saved files will probably fix that)
--- paint_app.py
+++ paint_app.py
@@ -1,67 +1,74 @@
-import tkinter
 import tkinter as tk
-from PIL import Image
-import io
-from tkinter.filedialog import asksaveasfile
 from tkinter import colorchooser
 
-
 window = tk.Tk()
-window.title("Paint")
+window.title("Paint!")
 
 canvas = tk.Canvas(window, width=600, height=600, background="white")
-
+canvas.grid_configure(row=1, column=0, columnspan=15)
 canvas.is_mouse_down = False
-canvas.prev_x = 0
-canvas.prev_y = 0
+canvas.color = "#000000"
+canvas.mode = "pen"
+canvas.prev_mouse_x = 0
+canvas.prev_mouse_y = 0
 
+size_slider = tk.Scale(window, from_=2, to_=25, orient="horizontal")
+size_slider.set(12)
+size_slider.grid_configure(row=0, column=0)
 
-def mouse_down(event: tkinter.Event):
-    canvas.is_mouse_down = True
-    canvas.prev_x = event.x
-    canvas.prev_y = event.y
-    draw_circle(canvas.prev_x, canvas.prev_y)
+def mouse_down(event):
+    w = size_slider.get()
+    if canvas.mode == "smiley":
+        canvas.create_text(event.x, event.y, text=":)", angle=270, font=("Arial", 4*w), fill=canvas.color)
+        canvas.is_mouse_down = True
+    else:
+        canvas.is_mouse_down = True
+        canvas.prev_mouse_x = event.x
+        canvas.prev_mouse_y = event.y
+        draw_circle(event.x, event.y)
 
 
 def mouse_up(event):
     canvas.is_mouse_down = False
 
 
-def mouse_motion(event):
+def mouse_move(event):
     if canvas.is_mouse_down:
-        w = width_slider.get()
-        color = canvas.color
-        draw_circle(canvas.prev_x, canvas.prev_y)
-        canvas.create_line(canvas.prev_x, canvas.prev_y, event.x, event.y, fill=color, width=w)
-        draw_circle(event.x, event.y)
-        canvas.prev_x = event.x
-        canvas.prev_y = event.y
+        width = size_slider.get()
+        if canvas.mode == "smiley":
+            draw_smiley(event.x, event.y)
+        else:
+            canvas.create_line(canvas.prev_mouse_x, canvas.prev_mouse_y, event.x, event.y, fill=get_draw_color(), width=width)
+            draw_circle(event.x, event.y)
 
-def draw_circle(x, y):
-    w = width_slider.get()
-    color = canvas.color
-    canvas.create_oval(x - w / 2, y - w / 2, x + w / 2 - 1, y + w / 2 - 1, fill=color, width=0)
+            canvas.prev_mouse_x = event.x
+            canvas.prev_mouse_y = event.y
 
 
 canvas.bind("<Button-1>", mouse_down)
-canvas.bind('<ButtonRelease-1>', mouse_up)
-canvas.bind('<Motion>', mouse_motion)
+canvas.bind("<ButtonRelease-1>", mouse_up)
+canvas.bind("<Motion>", mouse_move)
 
 
-def save_canvas():
-    canvas.update()
-    f = asksaveasfile(initialfile='Image.jpg',
-                      defaultextension=".jpg", filetypes=[("All Files", "*.*"), ("Image", "*.jpg")])
-
-    ps = canvas.postscript(colormode='color')
-    img = Image.open(io.BytesIO(ps.encode('utf-8')))
-    img.save(f)
+def draw_circle(x, y):
+    width = size_slider.get()
+    canvas.create_oval(x - width/2, y - width/2, x + width/2 - 1, y + width/2 - 1, fill=get_draw_color(), width=0)
 
 
-save_button = tk.Button(text="Save", command=save_canvas)
+def draw_smiley(x, y):
+    width = size_slider.get()
+    canvas.create_text(x, y, text=":)", angle=270, font=("Arial", 4 * width), fill=get_draw_color())
 
 
-def get_color(title, default) -> str:
+def pick_color_pressed():
+    canvas.color = get_color("Brush color", "#FF0000")
+
+
+def pick_bg_color_pressed():
+    canvas.configure(background=get_color("Background", "#FFFFFF"))
+
+
+def get_color(title, default):
     picked = colorchooser.askcolor(title=title, initialcolor=default)
     if picked[1] is not None:
         return picked[1]
@@ -69,53 +76,53 @@ def get_color(title, default) -> str:
     return default
 
 
-def pick_color():
-    canvas.color = get_color("Pick brush color", "#FF0000")
-    pen_button.color = canvas.color
+pick_color_button = tk.Button(window, text="Brush color", command=pick_color_pressed)
+pick_color_button.grid_configure(row=0, column=1)
+
+pick_bg_color_button = tk.Button(window, text="Background color", command=pick_bg_color_pressed)
+pick_bg_color_button.grid_configure(row=0, column=5)
 
 
-def pick_color_bg():
-    bg = get_color("Pick background color", "#FFFFFF")
-    canvas.configure(background=bg)
-
-
-canvas.color = "#000000"
-pick_color_button = tk.Button(text="Brush color", command=pick_color)
-pick_background_button = tk.Button(text="BG color", command=pick_color_bg)
-
-
-def pick_pen():
-    canvas.color = pen_button.color
-    pen_button["state"] = "disabled"
-    eraser_button["state"] = "normal"
-
-
-def pick_eraser():
-    canvas.color = "#FFFFFF"
-    pen_button["state"] = "normal"
-    eraser_button["state"] = "disabled"
+def scale():
+    canvas.scale("all", 0, 0, 2, 2)
+    canvas.configure(width=1200, height=1200)
 
 
 def clear_all():
     canvas.delete("all")
 
-pen_button = tk.Button(text="Pen", command=pick_pen)
-pen_button.color = "#000000"
 
-eraser_button = tk.Button(text="Eraser", command=pick_eraser)
-erase_all_button = tk.Button(text="Erase all", command=clear_all)
-pick_pen()
-
-width_slider = tk.Scale(window, from_=2, to_=25, orient="horizontal")
+erase_all_button = tk.Button(window, text="Erase all", command=clear_all)
+erase_all_button.grid_configure(row=0, column=6)
 
 
-pick_background_button.grid_configure(row=0, column=0)
-pick_color_button.grid_configure(row=0, column=1)
-pen_button.grid_configure(row=0, column=2)
-width_slider.grid_configure(row=0, column=3)
-eraser_button.grid_configure(row=0, column=4)
-erase_all_button.grid_configure(row=0, column=5)
-save_button.grid_configure(row=0, column=6)
-canvas.grid_configure(row=1, columnspan=10)
+def pick_eraser():
+    canvas.mode="eraser"
+
+
+def pick_pen():
+    canvas.mode = "pen"
+
+
+def pick_smiley():
+    canvas.mode = "smiley"
+
+
+pick_eraser_button = tk.Button(window, text="Eraser", command=pick_eraser)
+pick_eraser_button.grid_configure(row=0, column=2)
+
+pick_pen_button = tk.Button(window, text="Pen", command=pick_pen)
+pick_pen_button.grid_configure(row=0, column=3)
+
+pick_pen_button = tk.Button(window, text=":)", command=pick_smiley)
+pick_pen_button.grid_configure(row=0, column=4)
+
+
+def get_draw_color() -> str:
+    if canvas.mode == "eraser":
+        return "#FFFFFF"
+
+    return canvas.color
+
 
 window.mainloop()
